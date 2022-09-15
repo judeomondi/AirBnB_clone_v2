7c9f61e814b52ee603eab3be565341a1ad49ddab
jomondi <jude.omondi@cellulant.io>
2022-09-15 21:19:37 +0300

    task 0

---
+++ models/amenity.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" State Module for HBNB project """
+from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+
+
+class Amenity(BaseModel, Base):
+    """ The amenity class """
+
+    __tablename__ = "amenities"
+
+    if getenv('HBNB_TYPE_STORAGE') == "db":
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
